c19ce11a52115d7429ce4026d031ce3dcf8d1215
the-gsd <dvgidwani@gmail.com>
2025-03-27 19:15:07 +0530

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-red-500"
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
